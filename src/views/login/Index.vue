a50c945bb2a7f84bbc18b0f20491bc38e741b8da
xuxl <emialxuxl@163.com>
2020-01-13 22:35:03 +0800

    构建项目，路由，全局样式，elementui引入

---
+++ src/views/login/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>this is login page!
+         <el-button type="success">成功按钮</el-button>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
