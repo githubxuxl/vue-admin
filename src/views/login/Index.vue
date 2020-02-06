b8882d79ca26b6e0624979e60841590c5f13ac34
xuxl <emialxuxl@163.com>
2020-02-06 18:57:37 +0800

    class绑定

--- src/views/login/Index.vue
+++ src/views/login/Index.vue
@@ -1,30 +1,50 @@
 <template>
-    <div>
-        this is login page!
-    </div>
+  <div>
+    <br />
+    <el-button type="primary" size="small" @click="flag=!flag">button</el-button>
+    <div v-if="flag" @click="handleClick" :class="{ red:isRed, border:true }">test ele</div>
+    <div :class="classObject">test two</div>
+    <div :class="['red','border']">test three</div>
+  </div>
 </template>
 <script>
-import { stripScript } from '@/util/validate.js';
+import { stripScript } from "@/util/validate.js";
 
 export default {
-    name:'login',
-    components:{},
-    data(){
-        return{
-
-        }
-    },
-    created(){
-
-    },
-    mounted(){
-
-    },
-    methods:{
-        
+  name: "login",
+  components: {},
+  data() {
+    return {
+      flag: true,
+      isRed: true,
+      hasBorder: true,
+      classObject: {
+        red: true,
+        border: true,
+        "font-size18": true
+      }
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    handleClick: function() {
+      alert("haha");
     }
-}
+  }
+};
 </script>
 <style scoped>
+.red {
+  color: red;
+}
 
+.border {
+  width: 100px;
+  border: 1px solid gold;
+}
+
+.font-size18 {
+  font-size: 18px;
+}
 </style>
